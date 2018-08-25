061aed15705fc88688d7b17cc49148513d1f8034
young.huang <young.huang@jfz.com>
2018-08-25 15:30:44 +0800

    feat : merge

---
+++ Example/Pods/Target Support Files/YangBlurView/YangBlurView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_YangBlurView : NSObject
+@end
+@implementation PodsDummy_YangBlurView
+@end
--- Example/YangBlurView/ViewController.swift
+++ Example/YangBlurView/ViewController.swift
@@ -7,18 +7,31 @@
 //
 
 import UIKit
+import YangBlurView
 
 class ViewController: UIViewController {
 
+    let blurView = DynamicBlurView()
+    let progressBar = UISlider(frame: CGRect(x: 0, y: 0, width: 320, height: 100))
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.addSubview(progressBar)
+        progressBar.addTarget(self, action: #selector(progressValueChange), for: .valueChanged)
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 200, width: 320, height: 150))
+        imageView.image = #imageLiteral(resourceName: "bg_1")
+        self.view.addSubview(imageView)
+        self.view.backgroundColor = UIColor.black
+        blurView.frame = CGRect(x: 0, y: 200, width: 320, height: 150)
+
+        view.addSubview(blurView)
+//        blurView.blurRadius = 20
+//        blurView.blurRatio = 0.5
+    }
+    
+    @objc func progressValueChange() {
+        blurView.blurRadius = CGFloat(progressBar.value)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 }
 
